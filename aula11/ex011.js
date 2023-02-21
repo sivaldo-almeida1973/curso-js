68ca38db2f3b544ce5ea3519b88fe9c8ba3218cc
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-21 12:21:50 -0300

    Update ex011.js

---
+++ aula11/ex011.js
@@ -0,0 +1,8 @@
+//console.log('O console funcionou corretamente')
+
+var vel = 60.1
+console.log(`a velocidade do seu carro é ${vel} KM /h`)
+if (vel > 60){ //condição simples!
+  console.log('Voce ultrapassou a velocidade permitida .MULTADO!!')
+}
+console.log(`Dirija com cuidado e usando o cinto de segurança!`)
