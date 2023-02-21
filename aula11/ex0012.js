537d894ef414ef08d9396ce11f597e84e79f30fe
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-21 12:21:47 -0300

    Create ex0012.js

---
+++ aula11/ex0012.js
@@ -0,0 +1,12 @@
+//situaçao composta
+ var pais = 'Portugal'
+
+ console.log(`Vivendo em ${pais}`)
+
+ if (pais == 'Brasil'){
+  console.log('Voce é Brasileiro!')
+ }
+ 
+ else {
+  console.log('Voce é Estrangeiro!')
+ }
