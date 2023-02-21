198522c9c89babcf7add34f1aacf0a39cbed9779
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-21 19:43:47 -0300

    Create ex014.js

---
+++ aula12/ex014.js
@@ -0,0 +1,11 @@
+var idade = 18
+console.log(`Voce tem ${idade} anos`)
+if (idade < 16 ){
+  console.log('Nao Vota')
+}else if (idade  < 18 || idade > 65 ) {
+    console.log('voto opcional')
+  } else {
+    console.log('Voto Obrigatorio')
+    }
+  
+  
