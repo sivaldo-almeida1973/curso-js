3b44d5e79bf2ed39f4c1ad18e4e2fa5408862806
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-23 19:08:37 -0300

    Create funcao01.js

---
+++ aula17funcoes/funcao01.js
@@ -0,0 +1,24 @@
+let res = parimpar(4)
+
+function parimpar(n){
+  if (n % 2 == 0){
+    return 'Par'
+  }else{
+    return 'Impar'
+  }
+}
+console.log(res)
+
+
+
+//pode ser assim tambem
+
+
+function parimpar(n){
+  if (n % 2 == 0){
+    return 'Par'
+  }else{
+    return 'Impar'
+  }
+}
+console.log(parimpar(1))
