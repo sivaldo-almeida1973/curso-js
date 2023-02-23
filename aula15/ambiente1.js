5103702d084888a9fe9bec71f97d91872f3c28f5
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-23 11:39:13 -0300

    Create ambiente1.js

---
+++ aula15/ambiente1.js
@@ -0,0 +1,25 @@
+/*
+var c = 1
+while(c <10){  //enquanto c for menor que 10 repete comer fatia
+  console.log(`comer fatia ${c}`)
+  c += 1
+}
+*/
+
+//=========================
+//    inic   /  test log/ incremento  
+for (var c = 1; c <=1; c++){
+
+  console.log(`comer pizza ${c}`)
+}
+console.log('==================')
+
+
+//=========================
+
+console.log('vai começar...')
+
+for(var c = 1 ; c <=10; c++){
+  console.log(`começou ${c}`)
+}
+console.log('Fim!!')
