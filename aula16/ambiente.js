e0deeb1b32ac56ebe84ed682639bb060ba984cc1
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-23 16:58:51 -0300

    Create ambiente.js

---
+++ aula16/ambiente.js
@@ -0,0 +1,13 @@
+//variaveis compostas (array ou vetor)
+
+let num = [1,2,3,45,6]
+num[5] = 9 //incluir 100 no indice 6
+num.push(10) // incluir  o 105 no final
+num.length         //tamanho do array
+num.sort()
+console.log(`Nosso vetor é o ${num}`)
+console.log(`Nosso vetor tem  ${num.length} posições`)
+console.log(`O primeiro valor do vetor é  ${num[0]} posições`)
+console.log(num[0]) // mostrar elemento no indice 0
+
+console.log('=============')
