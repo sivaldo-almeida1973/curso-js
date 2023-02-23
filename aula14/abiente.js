8bc9d62838d203ce10dc29da4766b9b3d7c79e01
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-02-23 11:39:10 -0300

    Create abiente.js

---
+++ aula14/abiente.js
@@ -0,0 +1,34 @@
+/* estrutura de repetição com teste logico no inicio(while)
+var c = 1
+while (c < 8){
+  console.log('tudo bem')
+  c++ // c = c + 1
+}
+*/
+
+/*
+var c = 1           //inicialização
+while (c <= 8){     //teste logico
+  console.log(`comer fatia pizza ${c}`)
+  c++               //incremento
+}
+*/
+
+/*estrututra de repetição com teste logico no final(do while)*/
+
+var c = 1
+do{
+  console.log(`comer fatia pizza ${c}`)
+  c++
+}while (c <= 8)
+
+
+/*
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+*/
